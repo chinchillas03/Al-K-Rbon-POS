1b4691b4d60ea387abbcc67c9b1cb7b9f64f806b
Chinchillas <79560217+chinchillas03@users.noreply.github.com>
2024-10-09 19:02:50 -0700

    Producto, Extra y Producto Extra mapeado
    Recordar de cambiar la contraseña de persistencia a la de cada quien

---
+++ Al-KRbon_Persistencia/src/main/java/com/itson/al/krbon_persistencia/AlKRbon_Persistencia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.itson.al.krbon_persistencia;
+
+/**
+ *
+ * @author Usuario
+ */
+public class AlKRbon_Persistencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
