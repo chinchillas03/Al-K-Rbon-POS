83e740d63badb56003aa36576778c8a9695dce1d
Chinchillas <79560217+chinchillas03@users.noreply.github.com>
2024-10-10 14:03:14 -0700

    Agregamos el dominio

---
+++ AlKRbon_Dominio/src/main/java/com/itson/alkrbon_dominio/AlKRbon_Dominio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.itson.alkrbon_dominio;
+
+/**
+ *
+ * @author Usuario
+ */
+public class AlKRbon_Dominio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ AlKRbon_Dominio/src/main/java/org/itson/dominio/Extra.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.itson.dominio;
+
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+/**
+ *
+ * @author Usuario
+ */
+@Entity
+public class Extra {
+    
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "descripcion", nullable = false, length = 80)
+    private String descripcion;
+    @Column(name = "precioExtra", nullable = false, length = 10)
+    private double precioExtra;
+    @ManyToMany(mappedBy = "extras")
+    private List<Producto> productos;
+
+    public Extra() {
+    }
+
+    public Extra(Long id, String descripcion, double precioExtra) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.precioExtra = precioExtra;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecioExtra() {
+        return precioExtra;
+    }
+
+    public void setPrecioExtra(double precioExtra) {
+        this.precioExtra = precioExtra;
+    }
+    
+}
