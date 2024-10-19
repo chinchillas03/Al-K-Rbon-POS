a3ddb787ed05b79e573e004ac6d559a42e3c9b02
Chinchillas <79560217+chinchillas03@users.noreply.github.com>
2024-10-18 19:19:33 -0700

    Capa de presentacion y eliminacion de paquete que no se usa

---
+++ Al-Krbon_Presentacion/src/main/java/com/itson/al/krbon_presentacion/AlKrbon_Presentacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.itson.al.krbon_presentacion;
+
+/**
+ *
+ * @author Usuario
+ */
+public class AlKrbon_Presentacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
